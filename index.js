22eb68a0b24abe633779b84bc438a82db82029e0
German Matheus <germanmatheus.dev@gmail.com>
2025-02-22 00:16:50 -0400

    using a promise for reading files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const fs = require('fs')
+const fs = require('node:fs/promises')
 
 app.use(cors())
 const { extractMetadata, extractContent } = require('./utils')
@@ -9,10 +9,13 @@ const { extractMetadata, extractContent } = require('./utils')
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.get('/posts/:filename', (req, res) => {
-  let fileContent = fs.readFileSync(__dirname + '/posts/' + req.params.filename + '.mdx', 'utf8')
+app.get('/posts/:filename', async (req, res) => {
+  let fileContent = await fs.readFile(__dirname + '/posts/' + req.params.filename + '.mdx', 'utf8')
   let metadata = extractMetadata(fileContent)
   let content = extractContent(fileContent)
+
+
+  console.log({metadata, content})
   res.json({metadata, content})
 })
 app.listen(3000, () => {
